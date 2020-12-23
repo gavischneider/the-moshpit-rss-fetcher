be5f4898742f31bd40979e4754dfff546aa1dc71
Gavi Schneider <gavischneider@gmail.com>
2020-12-23 12:41:26 +0200

    Added eslint

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import { resolveSoa } from "dns";
+import express, {
+  Application,
+  Request,
+  Response,
+  NextFunction,
+  Router,
+} from "express";
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app: Application = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5000"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow seesion cookie from browser to come through
+  })
+);
+
+const port = process.env.PORT || 666;
+
+const connectionString = process.env.MONGO_CONNECTION_STRING;
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to database"))
+  .catch((err: Error) => console.log(`There was an error: ${err}`));
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Home Route");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = app;
---
+++ src/constants/feeds.ts
@@ -0,0 +1,13 @@
+export const feeds = [
+  {
+    name: "Loudwire",
+    url: "https://loudwire.com/feed/",
+    image:
+      "https://townsquare.media/site/366/files/2012/11/lw-logo_white-no-shadow-1462.png",
+  },
+  {
+    name: "MetalSucks",
+    url: "http://feeds.feedburner.com/Metalsucks",
+    image: "https://i.imgur.com/hwz9zvL.jpg",
+  },
+];
