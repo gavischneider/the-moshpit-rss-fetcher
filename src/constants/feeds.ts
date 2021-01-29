a08c5af6c556f3bf84717f36a5bb6d63087ae1a0
Gavi Schneider <gavischneider@gmail.com>
2021-01-29 12:36:06 +0200

    Updated webpack and babel config

--- src/constants/feeds.ts
+++ src/constants/feeds.ts
@@ -1,178 +1,178 @@
-export const feeds = [
-  {
-    name: "Loudwire",
-    url: "https://loudwire.com/feed/",
-    image: "https://townsquare.media/wp-content/uploads/2019/05/loudwire.png",
-  },
-  {
-    name: "MetalSucks",
-    url: "http://feeds.feedburner.com/Metalsucks",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_tepo1mGtGLNkKHD6fTFs_gz_pQSq_1Avig&usqp=CAU",
-  },
-  {
-    name: "Metal Injection",
-    url: "http://feeds.feedburner.com/metalinjection",
-    image:
-      "https://pbs.twimg.com/profile_images/1214639788566941699/hFRxL1Xz.jpg",
-  },
-  {
-    name: "BraveWords",
-    url: "https://bravewords.com/rss/news.xml",
-    image:
-      "https://pbs.twimg.com/profile_images/492791711987298304/vm2q1MQM_400x400.jpeg",
-  },
-  {
-    name: "Louder",
-    url: "http://www.loudersound.com/feeds/all",
-    image:
-      "https://pbs.twimg.com/profile_images/978989983439626240/vV-nqAVT_400x400.jpg",
-  },
-  {
-    name: "Invisible Oranges",
-    url: "invisibleoranges.com/feed/",
-    image: "https://www.google.com/s2/favicons?domain=invisibleoranges.com",
-  },
-  {
-    name: "Angry Metal Guy",
-    url: "https://www.angrymetalguy.com/feed/",
-    image:
-      "https://www.angrymetalguy.com/wp-content/uploads/2013/11/amg-mfi-300.jpg",
-  },
-  {
-    name: "No Clean Singing",
-    url: "https://www.nocleansinging.com/feed/",
-    image:
-      "https://img.discogs.com/1rP3kViX0styPlfTcFNeOs4M4gQ=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/L-454101-1371178416-9944.jpeg.jpg",
-  },
-  {
-    name: "Noisecreep",
-    url: "https://noisecreep.com/feed",
-    image:
-      "https://pbs.twimg.com/profile_images/344513261578345106/d9118695cff34df6e065d484797ce133_400x400.jpeg",
-  },
-  {
-    name: "Brooklyn Vegan",
-    url: "https://www.brooklynvegan.com/category/music/metal/feed/",
-    image:
-      "https://static1.squarespace.com/static/58eeff5517bffcc75c06153d/58ef16d46b8f5b2252ed9952/5c9b0d79fa0d603efa9aef0b/1553665569874/bv.jpg?format=1500w",
-  },
-  {
-    name: "Metal Talk",
-    url: "https://www.metaltalk.net/feed",
-    image:
-      "https://pbs.twimg.com/profile_images/1221872725960151040/00zZTc3T_400x400.jpg",
-  },
-  {
-    name: "AntiHero",
-    url: "https://www.antiheromagazine.com/music-news/feed/",
-    image:
-      "https://pbs.twimg.com/profile_images/898909302219431936/FCe3ciQp.jpg",
-  },
-  {
-    name: "The Rockpit",
-    url: "http://www.therockpit.net/feed/",
-    image:
-      "https://www.therockpit.net/wp-content/uploads/2020/09/TheRockpit-logo2020-graybg-small.jpg",
-  },
-  {
-    name: "Heavy Magazine",
-    url: "https://heavymag.com.au/feed/",
-    image:
-      "https://pbs.twimg.com/profile_images/1057897610814545920/ehycuqEy_400x400.jpg",
-  },
-  {
-    name: "Metal Bandcamp",
-    url: "http://feeds.metalbandcamp.com/metalbandcamp",
-    image: "https://f4.bcbits.com/img/0001492584_20.jpg",
-  },
-  {
-    name: "From the Dust Returned",
-    url: "http://autothrall.blogspot.com/feeds/posts/default",
-    image:
-      "https://1.bp.blogspot.com/-u8g4_LVleFc/X6tDW2FshsI/AAAAAAAASOs/29E-Whw8JTQuv13IC6o-_v1ak-9LSTipgCK4BGAYYCw/s400/wolf-of-ansbach.jpg",
-  },
-  {
-    name: "Tape Wyrm",
-    url: "https://tapewyrmmetal.com/feed",
-    image:
-      "https://tapewyrmmetal.com/wp-content/uploads/2014/02/cropped-dragongif1.gif",
-  },
-  {
-    name: "Gear Gods",
-    url: "http://feeds.feedburner.com/geargods",
-    image:
-      "https://pbs.twimg.com/profile_images/378800000426281192/2f79380f9756f29ea094e5fa832fcf4f.jpeg",
-  },
-  {
-    name: "Metantoine's Magikal Realm",
-    url: "http://metantoinemagicalrealm.blogspot.com/feeds/posts/default",
-    image:
-      "http://3.bp.blogspot.com/-b_SI79xy3VQ/XoE8jxHcLkI/AAAAAAAAab4/ra-e0slSSwIB6V9mkRMXaXulgN4qzMUlQCK4BGAYYCw/s1600/banner_02.png",
-  },
-  {
-    name: "Metal Temple",
-    url: "http://www.metal-temple.com/backend.php?mode=news&type=rss",
-    image:
-      "https://pbs.twimg.com/profile_images/598999875/metal-temple-logo_400x400.png",
-  },
-  {
-    name: "Meat Mead Metal",
-    url: "https://meatmeadmetal.com/feed/",
-    image:
-      "https://meatmeadmetal.files.wordpress.com/2013/08/cropped-cropped-meadmeatlogov2_horizontal-712x148-xxx.jpg",
-  },
-  {
-    name: "Metal Obsession",
-    url: "http://www.metalobsession.net/feed/",
-    image: "http://www.metalobsession.net/wp-content/uploads/2013/04/mo.jpg",
-  },
-  {
-    name: "Death Metal Underground",
-    url: "http://www.deathmetal.org/feed/",
-    image:
-      "https://pbs.twimg.com/profile_images/2563469648/death_metal_underground-180px_400x400.png",
-  },
-  {
-    name: "Technical Death Metal",
-    url: "https://technicaldeathmetal.org/feed/",
-    image:
-      "https://pbs.twimg.com/profile_images/828197499575869441/frRSYcWM.jpg",
-  },
-  {
-    name: "Transcended Music Blog",
-    url: "https://www.transcendedmusic.de/feed/",
-    image:
-      "https://pbs.twimg.com/profile_images/1251249459171864576/Sc5MR7v0_400x400.jpg",
-  },
-  {
-    name: "Black Metal and Brews",
-    url: "http://blackmetalandbrews.blogspot.com/feeds/posts/default?alt=rss",
-    image:
-      "https://pbs.twimg.com/profile_images/1346646605093548041/CopdJaeR_400x400.jpg",
-  },
-  {
-    name: "Metallic Imagery",
-    url: "http://metallicimagery.blogspot.com/feeds/posts/default?alt=rss",
-    image:
-      "https://3.bp.blogspot.com/-zY7K70qNjkk/ViRNLNwUZeI/AAAAAAAAB2w/9slDdSx4s4w/s660/Screen%2BShot%2B2015-10-18%2Bat%2B9.20.18%2BPM.png",
-  },
-  {
-    name: "Metal Riot",
-    url: "http://feeds.feedburner.com/MetalRiot",
-    image:
-      "https://scontent.fsdv3-1.fna.fbcdn.net/v/t1.0-9/93301987_2976643622375049_5611497337705725952_n.jpg?_nc_cat=106&ccb=2&_nc_sid=174925&_nc_ohc=QXMTmg62awEAX9NRr1Y&_nc_ht=scontent.fsdv3-1.fna&oh=1662fb656c3fdf9e3204adff58ce3620&oe=602EA9D9",
-  },
-  {
-    name: "MegaDepth",
-    url: "https://mega-depth.com/feed/",
-    image:
-      "https://pbs.twimg.com/profile_images/896484600247201793/WDD-64xd.jpg",
-  },
-  {
-    name: "Echoes and Dust",
-    url: "https://echoesanddust.com/feed/",
-    image:
-      "https://pbs.twimg.com/profile_images/972114542875602944/iYGORSvZ.jpg",
-  },
-];
+// export const feeds = [
+//   {
+//     name: "Loudwire",
+//     url: "https://loudwire.com/feed/",
+//     image: "https://townsquare.media/wp-content/uploads/2019/05/loudwire.png",
+//   },
+//   {
+//     name: "MetalSucks",
+//     url: "http://feeds.feedburner.com/Metalsucks",
+//     image:
+//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_tepo1mGtGLNkKHD6fTFs_gz_pQSq_1Avig&usqp=CAU",
+//   },
+//   {
+//     name: "Metal Injection",
+//     url: "http://feeds.feedburner.com/metalinjection",
+//     image:
+//       "https://pbs.twimg.com/profile_images/1214639788566941699/hFRxL1Xz.jpg",
+//   },
+//   {
+//     name: "BraveWords",
+//     url: "https://bravewords.com/rss/news.xml",
+//     image:
+//       "https://pbs.twimg.com/profile_images/492791711987298304/vm2q1MQM_400x400.jpeg",
+//   },
+//   {
+//     name: "Louder",
+//     url: "http://www.loudersound.com/feeds/all",
+//     image:
+//       "https://pbs.twimg.com/profile_images/978989983439626240/vV-nqAVT_400x400.jpg",
+//   },
+//   {
+//     name: "Invisible Oranges",
+//     url: "invisibleoranges.com/feed/",
+//     image: "https://www.google.com/s2/favicons?domain=invisibleoranges.com",
+//   },
+//   {
+//     name: "Angry Metal Guy",
+//     url: "https://www.angrymetalguy.com/feed/",
+//     image:
+//       "https://www.angrymetalguy.com/wp-content/uploads/2013/11/amg-mfi-300.jpg",
+//   },
+//   {
+//     name: "No Clean Singing",
+//     url: "https://www.nocleansinging.com/feed/",
+//     image:
+//       "https://img.discogs.com/1rP3kViX0styPlfTcFNeOs4M4gQ=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/L-454101-1371178416-9944.jpeg.jpg",
+//   },
+//   {
+//     name: "Noisecreep",
+//     url: "https://noisecreep.com/feed",
+//     image:
+//       "https://pbs.twimg.com/profile_images/344513261578345106/d9118695cff34df6e065d484797ce133_400x400.jpeg",
+//   },
+//   {
+//     name: "Brooklyn Vegan",
+//     url: "https://www.brooklynvegan.com/category/music/metal/feed/",
+//     image:
+//       "https://static1.squarespace.com/static/58eeff5517bffcc75c06153d/58ef16d46b8f5b2252ed9952/5c9b0d79fa0d603efa9aef0b/1553665569874/bv.jpg?format=1500w",
+//   },
+//   {
+//     name: "Metal Talk",
+//     url: "https://www.metaltalk.net/feed",
+//     image:
+//       "https://pbs.twimg.com/profile_images/1221872725960151040/00zZTc3T_400x400.jpg",
+//   },
+//   {
+//     name: "AntiHero",
+//     url: "https://www.antiheromagazine.com/music-news/feed/",
+//     image:
+//       "https://pbs.twimg.com/profile_images/898909302219431936/FCe3ciQp.jpg",
+//   },
+//   {
+//     name: "The Rockpit",
+//     url: "http://www.therockpit.net/feed/",
+//     image:
+//       "https://www.therockpit.net/wp-content/uploads/2020/09/TheRockpit-logo2020-graybg-small.jpg",
+//   },
+//   {
+//     name: "Heavy Magazine",
+//     url: "https://heavymag.com.au/feed/",
+//     image:
+//       "https://pbs.twimg.com/profile_images/1057897610814545920/ehycuqEy_400x400.jpg",
+//   },
+//   {
+//     name: "Metal Bandcamp",
+//     url: "http://feeds.metalbandcamp.com/metalbandcamp",
+//     image: "https://f4.bcbits.com/img/0001492584_20.jpg",
+//   },
+//   {
+//     name: "From the Dust Returned",
+//     url: "http://autothrall.blogspot.com/feeds/posts/default",
+//     image:
+//       "https://1.bp.blogspot.com/-u8g4_LVleFc/X6tDW2FshsI/AAAAAAAASOs/29E-Whw8JTQuv13IC6o-_v1ak-9LSTipgCK4BGAYYCw/s400/wolf-of-ansbach.jpg",
+//   },
+//   {
+//     name: "Tape Wyrm",
+//     url: "https://tapewyrmmetal.com/feed",
+//     image:
+//       "https://tapewyrmmetal.com/wp-content/uploads/2014/02/cropped-dragongif1.gif",
+//   },
+//   {
+//     name: "Gear Gods",
+//     url: "http://feeds.feedburner.com/geargods",
+//     image:
+//       "https://pbs.twimg.com/profile_images/378800000426281192/2f79380f9756f29ea094e5fa832fcf4f.jpeg",
+//   },
+//   {
+//     name: "Metantoine's Magikal Realm",
+//     url: "http://metantoinemagicalrealm.blogspot.com/feeds/posts/default",
+//     image:
+//       "http://3.bp.blogspot.com/-b_SI79xy3VQ/XoE8jxHcLkI/AAAAAAAAab4/ra-e0slSSwIB6V9mkRMXaXulgN4qzMUlQCK4BGAYYCw/s1600/banner_02.png",
+//   },
+//   {
+//     name: "Metal Temple",
+//     url: "http://www.metal-temple.com/backend.php?mode=news&type=rss",
+//     image:
+//       "https://pbs.twimg.com/profile_images/598999875/metal-temple-logo_400x400.png",
+//   },
+//   {
+//     name: "Meat Mead Metal",
+//     url: "https://meatmeadmetal.com/feed/",
+//     image:
+//       "https://meatmeadmetal.files.wordpress.com/2013/08/cropped-cropped-meadmeatlogov2_horizontal-712x148-xxx.jpg",
+//   },
+//   {
+//     name: "Metal Obsession",
+//     url: "http://www.metalobsession.net/feed/",
+//     image: "http://www.metalobsession.net/wp-content/uploads/2013/04/mo.jpg",
+//   },
+//   {
+//     name: "Death Metal Underground",
+//     url: "http://www.deathmetal.org/feed/",
+//     image:
+//       "https://pbs.twimg.com/profile_images/2563469648/death_metal_underground-180px_400x400.png",
+//   },
+//   {
+//     name: "Technical Death Metal",
+//     url: "https://technicaldeathmetal.org/feed/",
+//     image:
+//       "https://pbs.twimg.com/profile_images/828197499575869441/frRSYcWM.jpg",
+//   },
+//   {
+//     name: "Transcended Music Blog",
+//     url: "https://www.transcendedmusic.de/feed/",
+//     image:
+//       "https://pbs.twimg.com/profile_images/1251249459171864576/Sc5MR7v0_400x400.jpg",
+//   },
+//   {
+//     name: "Black Metal and Brews",
+//     url: "http://blackmetalandbrews.blogspot.com/feeds/posts/default?alt=rss",
+//     image:
+//       "https://pbs.twimg.com/profile_images/1346646605093548041/CopdJaeR_400x400.jpg",
+//   },
+//   {
+//     name: "Metallic Imagery",
+//     url: "http://metallicimagery.blogspot.com/feeds/posts/default?alt=rss",
+//     image:
+//       "https://3.bp.blogspot.com/-zY7K70qNjkk/ViRNLNwUZeI/AAAAAAAAB2w/9slDdSx4s4w/s660/Screen%2BShot%2B2015-10-18%2Bat%2B9.20.18%2BPM.png",
+//   },
+//   {
+//     name: "Metal Riot",
+//     url: "http://feeds.feedburner.com/MetalRiot",
+//     image:
+//       "https://scontent.fsdv3-1.fna.fbcdn.net/v/t1.0-9/93301987_2976643622375049_5611497337705725952_n.jpg?_nc_cat=106&ccb=2&_nc_sid=174925&_nc_ohc=QXMTmg62awEAX9NRr1Y&_nc_ht=scontent.fsdv3-1.fna&oh=1662fb656c3fdf9e3204adff58ce3620&oe=602EA9D9",
+//   },
+//   {
+//     name: "MegaDepth",
+//     url: "https://mega-depth.com/feed/",
+//     image:
+//       "https://pbs.twimg.com/profile_images/896484600247201793/WDD-64xd.jpg",
+//   },
+//   {
+//     name: "Echoes and Dust",
+//     url: "https://echoesanddust.com/feed/",
+//     image:
+//       "https://pbs.twimg.com/profile_images/972114542875602944/iYGORSvZ.jpg",
+//   },
+// ];
